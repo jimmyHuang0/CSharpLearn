92d590a7f633060a7e0dafe72eb9f3ee1b8d8d66
jimmy <jianming.huang@intelligentgrup.cn>
2018-03-12 10:25:24 +0800

    增加串口基本读写模块

---
+++ program/AOPUnity/MyAOP/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAOP
+{
+    /// <summary>
+    /// 1 AOP面向切面编程
+    /// 2 静态代理实现AOP
+    /// 3 动态代理实现AOP
+    /// 4 Unity、MVC中的AOP
+    /// 
+    /// POP
+    /// 
+    /// OOP：面向对象编程，万物皆对象，类与类交互，组成功能/组件/系统，适合做大项目，大系统
+    ///      静态的，封闭的，任何需求的变化都会带来影响
+    ///      GOF23种设计模式 设计模式原则，解决类与类之间的交互稳定
+    ///      类的自身变化，增加功能需求，解决不了的
+    ///      
+    /// AOP：面向切面编程，实际上是对OOP的一种补充
+    ///      允许通过外部方式来修改对象，可以在不破坏类型封装的前提下，去提供新的功能
+    ///      只是增加通用逻辑，，比如事务、日志、异常、权限、校验、缓存
+    ///      
+    /// 反射  包一层  特性  filter  代理模式   装饰器模式   注入
+    /// 
+    /// AOP:装饰器/代理模式                静态AOP
+    ///     realproxy/emit生成/Unity       动态代理AOP
+    ///     mvc filter                     反射+统一入口，特性
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("欢迎来到.Net高级班vip课程，今天学习面向切面AOP");
+                #region AOP show
+               //Console.WriteLine("***********************");
+               //DecoratorAOP.Show();
+               //
+               //Console.WriteLine("***********************");
+               //ProxyAOP.Show();
+               //Console.WriteLine("***********************");
+               //CastleProxyAOP.Show();
+               //Console.WriteLine("***********************");
+                UnityAOP.Show();
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ program/serialDemo/ConsoleApp10/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComSerial com = new ComSerial();
+            com.Open();
+            byte[] data = { 0x01, 0x03, 0x00, 0x01, 0x00, 0x01, 0xD5, 0xCA };
+            
+
+            var s= Console.ReadLine();
+            while(s!="q")
+            {
+                com.send(data);
+                s = Console.ReadLine();
+            }
+
+
+            com.Close();
+
+            Console.ReadLine();
+        }
+
+       
+    }
+
+    class ComSerial
+    {
+        private SerialPort ComDevice = new SerialPort();
+
+        public ComSerial()
+        {
+            var names= SerialPort.GetPortNames();
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+
+            
+            ComDevice.DataReceived += new SerialDataReceivedEventHandler(Com_DataReceived);
+        }
+
+        public void Open()
+        {
+            ComDevice.PortName = "COM1";
+            ComDevice.BaudRate = 19200;
+            ComDevice.Parity = Parity.None;
+            ComDevice.DataBits =8;
+            ComDevice.StopBits = StopBits.One;
+            try
+            {
+                ComDevice.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, "未能成功开启串口");
+                return;
+            }
+        }
+        public void Close()
+        {
+            try
+            {
+                ComDevice.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, "未能成功关闭串口");
+                return;
+            }
+        }
+
+        public bool send(byte[] data)
+        {
+             if (ComDevice.IsOpen)
+             {
+                 try
+                 {
+                    
+                     //将消息传递给串口
+                     ComDevice.Write(data, 0, data.Length);
+                     return true;
+                 }
+                 catch (Exception ex)
+                 {
+                    Console.WriteLine(ex.Message, "发送失败");
+                 }
+             }
+             else
+             {
+                Console.WriteLine("串口未开启", "错误");
+             }
+             return false;
+         }
+
+
+        private void Com_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //开辟接收缓冲区
+            byte[] ReDatas = new byte[ComDevice.BytesToRead];
+            //从串口读取数据
+            ComDevice.Read(ReDatas, 0, ReDatas.Length);
+            //实现数据的解码与显示
+            AddData(ReDatas);
+        }
+
+        public void AddData(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sb.AppendFormat("{0:x2}" + " ", data[i]);
+            }
+            AddContent(sb.ToString().ToUpper());
+        }
+
+        private void AddContent(string content)
+        {
+            Console.WriteLine(content);
+        }
+
+
+    }
+}
